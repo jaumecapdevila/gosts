7ff4184bd213d4f52e7f333cd4dbda814f935d44
Jaume Capdevila <contact@jaumecapdevila.net>
2019-03-09 18:07:35 +0100

    Test reader

---
+++ test/os_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/jaumecapdevila/gosts/file"
+)
+
+const tmpFile = "/tmp/hosts"
+
+func TestReaderShouldSeeFileContent(t *testing.T) {
+	var err error
+
+	reader := file.NewOSReader()
+
+	f, err := os.Create(tmpFile)
+
+	f.Close()
+
+	if err != nil && !os.IsExist(err) {
+		t.Errorf("Unable to create the test file")
+		return
+	}
+
+	_, err = reader.Read(tmpFile, file.ASSERT)
+
+	if err != nil {
+		t.Errorf("Unable to read file '%s'", tmpFile)
+	}
+
+	os.Remove(tmpFile)
+}
